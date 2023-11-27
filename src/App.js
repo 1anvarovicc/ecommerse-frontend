376953f8135e3aba2f6277141e044534e0d7a437
ASROR <asrorxon0108@gmail.com>
2023-11-27 15:08:30 +0500

    login register

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+function App() {
+  return (
+   <Router>
+    <Routes>
+      <Route path='/login' element={<LoginPage />}/>
+      <Route path='/sign-up' element={<RegisterPage/>}/>
+    </Routes>
+   </Router>
+  );
+}
+
+export default App;
